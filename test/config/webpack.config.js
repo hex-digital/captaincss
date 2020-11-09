c34a3e92a68997d1d8669c008637bb0c3c3adb77
Jamie Warburton <jamie@hexdigital.com>
2020-11-09 08:45:09 +0000

    Add a test directory to easily test changes and provide a demo

---
+++ test/config/webpack.config.js
@@ -0,0 +1,93 @@
+const paths = require('./paths');
+
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Captaincss = require('../../src/captaincss');
+
+module.exports = {
+  // Where webpack looks to start building the bundle
+  entry: [paths.src + '/scss/main.scss'],
+
+  // Where webpack outputs the assets and bundles
+  output: {
+    path: paths.build,
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+
+  // Control how source maps are generated
+  devtool: 'inline-source-map',
+
+  // Spin up a server for quick development
+  devServer: {
+    historyApiFallback: true,
+    contentBase: paths.build,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+
+  // Customize the webpack build process
+  plugins: [
+    // Removes/cleans build folders and unused assets when rebuilding
+    new CleanWebpackPlugin(),
+
+    // Takes Tailwind's config and outputs it to SCSS to be used
+    new Captaincss(),
+
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+
+    // Extracts CSS into separate files
+    // Note: style-loader is for development, MiniCssExtractPlugin is for production
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
+
+    // Generates an HTML file from a template
+    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
+    new HtmlWebpackPlugin({
+      title: 'Test the Captain',
+      template: paths.src + '/index.html', // template file
+      filename: 'index.html', // output file
+    }),
+  ],
+
+  // Determine how modules within the project are treated
+  module: {
+    rules: [
+      // Styles: Inject CSS into the head with source maps
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1 },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                ident: 'postcss', // Needs an identity in order to use 'require' in the plugins
+                plugins: [require('tailwindcss'), require('autoprefixer')],
+              },
+            },
+          },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+
+      // Images: Copy image files to build folder
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+
+      // Fonts and SVGs: Inline files
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+    ],
+  },
+};
