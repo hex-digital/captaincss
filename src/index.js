e02a5814072e5ead1466de0eec84f6614aac17e6
Jamie Warburton <jamie@hexdigital.com>
2020-12-20 20:14:45 +0000

    feat: add intrinsic center utility

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const plugin = require('tailwindcss/plugin');
 const defaultConfig = require('./defaultConfig');
+
 const addClusterComponent = require('./objects/cluster');
 const addStackComponent = require('./objects/stack');
 const addWrapperComponent = require('./objects/wrapper');
+
+const addIntrinsicCenterUtility = require('./utilities/intrinsic-center');
 
 module.exports = plugin(function (params) {
   addClusterComponent(params);
   addStackComponent(params);
   addWrapperComponent(params);
+
+  addIntrinsicCenterUtility(params);
 }, defaultConfig);
