15cff58f54866dafacefbeac955bc811045dfe41
Jamie Warburton <jamie@hexdigital.com>
2020-12-30 21:37:51 +0000

    fix: fix for incorrect import in src/defaultConfig

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -1,5 +1,5 @@
 // This file is deprecated and due to be removed. Import '@captaincss/captaincss/defaultConfig` instead
 const cloneDeep = require('lodash/cloneDeep');
-const defaultConfig = require('./stubs/defaultConfig.stub.js');
+const defaultConfig = require('../stubs/defaultConfig.stub.js');
 
 module.exports = cloneDeep(defaultConfig);
